63128bb1cca918312f06ac39fe7ff33ffc5d55c7
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-06-02 12:50:55 -0700

    add TailwindCSS

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Stake•fish Crypto App</title>
         <meta name="description" content="Stake•fish Crypto App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Welcome to <a href="https://nextjs.org">FishStake.js!</a>
         </h1>
       </main>
 
