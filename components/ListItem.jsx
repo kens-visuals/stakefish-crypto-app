8f22ec4584888b98c79c7a6b31635c86be5bf1e2
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-06-03 20:54:37 -0700

    make changes to improve image loading

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -4,11 +4,21 @@ import Image from 'next/image';
 // components
 import SecondaryList from './SecondaryList';
 
+// assets
+import blurPlaceholder from '../assets/main-color.png';
+
 export default function ListItem({ coin }) {
   return (
     <li className="p-2 px-6 md:px-3">
       <div className="flex items-center gap-2">
-        <Image src={coin.image} alt={coin.name} width={18} height={18} />
+        <Image
+          src={coin.image}
+          alt={coin.name}
+          width={18}
+          height={18}
+          priority
+          blurDataURL={blurPlaceholder}
+        />
 
         <div className="flex items-center gap-2">
           <Link href="/coins/[coinId]" as={`/coins/${coin.id}`}>
